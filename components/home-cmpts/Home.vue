5948fae750a4c01050326de472f2ca76a8c5362c
MikeFranco <mfranco_98@yahoo.com>
2019-10-26 02:47:19 -0500

    we fixed the

--- components/home-cmpts/Home.vue
+++ components/home-cmpts/Home.vue
@@ -4,9 +4,13 @@
       <NavBar/> <!-- //TODO Hay que hacer este nav bar -->
     </div>
     <div>
-      <fa class="main-btn" :icon="[ 'fal', 'external-link-alt' ]" />
-      <fa class="main-btn" :icon="[ 'fal', 'external-link-alt' ]" />
-      <fa class="main-btn" :icon="[ 'fal', 'external-link-alt' ]" />
+      <!-- //TODO arreglar recorte de la imagen -->
+      <img class="first-screen-img" src="../../assets/FSI_1.png"> 
+    </div>
+    <div class="div-main-btns" >
+      <fa class="main-btn main-btn-left" :icon="[ 'fal', 'external-link-alt' ]" />
+      <fa class="main-btn main-btn-center" :icon="[ 'fal', 'external-link-alt' ]" />
+      <fa class="main-btn main-btn-right" :icon="[ 'fal', 'external-link-alt' ]" />
     </div>
   </section>
 </template>
@@ -27,6 +31,21 @@ export default {
 
 .main-btn {
   font-size: 50px;
+}
+
+.main-btn-center,
+.main-btn-right {
+  margin-left: 25%
+}
+
+.first-screen-img {
+  max-width: 100%;
+  max-height: 25%;
+}
+
+.div-main-btns {
+  text-align: center;
+  margin-bottom: 25%;
 }
 
 </style>
