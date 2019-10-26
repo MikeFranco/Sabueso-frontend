0de4cb75fa81c0b46de158db55fe41e51d6059ea
Mariana Ruiz de Chávez <mariixrch@MacBook-Pro-de-Mariana.local>
2019-10-26 10:59:01 -0500

    background hecho de el login

---
+++ pages/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <section> 
+        <div class="container">
+      <div class="opacidad"></div>
+    </div>
+    
+    </section>
+    
+</template> 
+
+<script>
+export default {
+    data(){
+        return {
+            bckImg: require('../../assets/login.jpg')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.container {
+  width: 100%;
+  height: 100vh;
+  background-image: url("../../assets/login.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: 0;
+}
+
+.opacidad {
+  background: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  height: 100%;
+}
+
+</style>
+
+
+
