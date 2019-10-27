777eca5c20417650d91b76685dd86706d2ab6c1e
MikeFranco <mfranco_98@yahoo.com>
2019-10-27 00:39:55 -0500

    fixed login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,117 +1,81 @@
 <template>
-    <section class="container"> 
-        <div class="opacidad">
-        <v-app id="inspire">
-    <v-form
-    class="form"
-      ref="form"
-      v-model="valid"
-      lazy-validation
-    >
-      <v-text-field
-        v-model="name"
-        :rules="nameRules"
-        label="Nombre"
-        required
-      ></v-text-field>
+  <section class="container">
+    <div class="opacidad">
+      <v-app id="inspire">
+        <v-form class="form" ref="form" v-model="valid" lazy-validation>
+          <v-text-field v-model="name" :rules="nameRules" label="Nombre" required></v-text-field>
 
-      <v-text-field
-        v-model="lastname"
-        :rules="lastnameRules"
-        label="Apellido"
-        required
-      ></v-text-field>
-  
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
-      
-      <v-text-field
-        v-model="celphone"
-        :rules="celphonelRules"
-        label="Celular"
-        required
-      ></v-text-field>
-    
-  
-      <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Talla"
-        required
-      ></v-select>
-  
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="¿Aceptas terminos y condiciones de nuestro aviso de privacidad?"
-        required
-      ></v-checkbox>
+          <v-text-field v-model="lastname" :rules="lastnameRules" label="Apellido" required></v-text-field>
 
-      
-  
-      <v-btn
-        color="#B21779"
-        class="mr-4"
-        @click="reset"
-      >
-        Reset Form
-      </v-btn>
-  
-    </v-form>
-  </v-app>
-        </div>
-        
-    </section>
-    
+          <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+          <v-text-field v-model="mobile" :rules="mobileRules" label="Celular" required></v-text-field>
+
+          <v-text-field type="password" v-model="password" :rules="passwordRules" label="Contraseña" required></v-text-field>
+
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to continue!']"
+            label="¿Aceptas terminos y condiciones de nuestro aviso de privacidad?"
+            required
+          ></v-checkbox>
+
+          <v-btn color="#B21779" class="mr-4" @click="reset">Reset Form</v-btn>
+        </v-form>
+      </v-app>
+    </div>
+  </section>
 </template> 
 
 <script>
 export default {
-    data(){
-        return {
-            bckImg: require('../../assets/login.jpg'), valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    select: null,
-    items: [
-      'Pequeña', 
-      'Mediana',
-      'Grande',
-    ],
-    checkbox: false,
-        }
-    }, methods: {
-    validate () {
+  data() {
+    return {
+      bckImg: require("../../assets/login.jpg"),
+      valid: true,
+      name: "",
+      lastname:'',
+      email: "",
+      mobile:'',
+      password:'',
+      select: null,
+      checkbox: false,
+      lastnameRules:[
+        v => !!v || "Last Name is required"
+      ],
+      nameRules: [
+        v => !!v || "Name is required",
+      ],
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ],
+      mobileRules:[
+        v => !!v || "Mobile is required",
+      ],
+      passwordRules:[
+        v => (v && v.length >= 8) || "Password must be more than 8 characters",
+        v => !!v || "Password is required",
+      ]
+    };
+  },
+  methods: {
+    validate() {
       if (this.$refs.form.validate()) {
-        this.snackbar = true
+        this.snackbar = true;
       }
     },
-    reset () {
-      this.$refs.form.reset()
+    reset() {
+      this.$refs.form.reset();
     },
-    resetValidation () {
-      this.$refs.form.resetValidation()
-    },
-  },
-}
-
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 .container {
   min-width: 100%;
   height: 100vh;
@@ -131,17 +95,17 @@ export default {
 }
 
 .form {
-    width: 100%;
-    height: 100%;
-    padding-right: 25%;
-    padding-left: 25%;
-    padding-top: 3%;
-    margin: 10px;
+  width: 100%;
+  height: 100%;
+  padding-right: 25%;
+  padding-left: 25%;
+  padding-top: 3%;
+  margin: 10px;
 }
 #inspire {
-    background: 0;}
-
-
+  background: 0;
+  padding-top: 3%
+}
 </style>
 
 
